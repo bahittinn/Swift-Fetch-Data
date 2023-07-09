77929865037aa341266785159dcd556d56cd1850
bahittinn <bahittin84@gmail.com>
2023-07-09 21:27:25 +0300

    deneme commit

--- Fetch-Search/Controller/detailViewController.swift
+++ Fetch-Search/Controller/detailViewController.swift
@@ -17,16 +17,8 @@ class detailViewController: UIViewController {
         print(character)
         animeLabel.text = anime
     }
-    
+    //detailviewController
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     
 
 }
